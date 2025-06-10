11722e2f91efd0e163e5fece84a1f747b898f255
Nelson <punkpunkada@gmail.com>
2025-06-10 10:16:47 -0300

    creating sign in component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { ConsultaComponent } from './consulta/consulta.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: CadastroComponent
+  },
+  {
+    path: 'consulta',
+    component: ConsultaComponent
+  }
+];
