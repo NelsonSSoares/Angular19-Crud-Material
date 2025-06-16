b15b86d9207f7a07d999cc775ba480898e47d488
Nelson <punkpunkada@gmail.com>
2025-06-16 12:25:19 -0300

    creating service to get and set in localstorage

--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -6,11 +6,22 @@ import { Cliente } from '../models/cliente/cliente';
 })
 export class ClienteService {
 
+  static REPO_CLIENTES = '_CLIENTES';
+
   constructor() { }
 
   salvar(cliente: Cliente): void {
     // Aqui você pode adicionar a lógica para salvar o cliente, como enviar para um serviço ou API
     console.log('Cliente salvo:', cliente);
+  }
 
+  obterStorage(): Cliente[] {
+    const clientesJson = localStorage.getItem(ClienteService.REPO_CLIENTES);
+    if (clientesJson) {
+      return JSON.parse(clientesJson) as Cliente[];
+    }
+    const clientes: Cliente[] = [];
+    localStorage.setItem(ClienteService.REPO_CLIENTES, JSON.stringify(clientes));
+    return clientes;
   }
 }
